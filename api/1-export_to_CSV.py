1c095c631f2789c1771abacfbe242462a8e9365c
cyrine <cyrinezekri4@gmail.com>
2023-11-24 23:04:16 +0100

    last attempt

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,21 +6,20 @@ import sys
 
 
 def export_tasks_to_csv(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com'
+    '''Export tasks to a CSV file for the given employee ID.'''
+    try:
+        base_url = 'https://jsonplaceholder.typicode.com'
 
-    # Fetch user data
-    user_response = requests.get(f'{base_url}/users/{employee_id}')
-    todos_response = requests.get(
-        f'{base_url}/todos',
-        params={
-            'userId': employee_id})
-
-    # Check if both requests were successful
-    if user_response.status_code == 200 and todos_response.status_code == 200:
+        user_response = requests.get(f'{base_url}/users/{employee_id}')
+        user_response.raise_for_status()
         user = user_response.json()
+
+        todos_response = requests.get(
+            f'{base_url}/todos',
+            params={'userId': employee_id})
+        todos_response.raise_for_status()
         todos = todos_response.json()
 
-        # Prepare data for CSV
         tasks_data = [
             ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
         ]
@@ -32,7 +31,6 @@ def export_tasks_to_csv(employee_id):
                 task['title']
             ])
 
-        # Write data to CSV
         csv_file_name = f"{employee_id}.csv"
         with open(csv_file_name, 'w', newline='') as csv_file:
             writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
@@ -40,11 +38,10 @@ def export_tasks_to_csv(employee_id):
 
         print(
             f"Tasks for employee {user['name']} have been exported to {csv_file_name}.")
-    else:
-        print("Failed to fetch data")
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
 
 
-# Command line argument check and script execution
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: Please enter a user ID.")
