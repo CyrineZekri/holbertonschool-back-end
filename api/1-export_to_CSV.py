eb6f750b856305b152b2c3969339d3d908f4f5ef
cyrine <cyrinezekri4@gmail.com>
2023-11-24 15:15:13 +0100

    with user name passed as an argument

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 ''' Module documentation'''
+import sys
 import csv
 import requests
 
@@ -15,17 +16,22 @@ def export_tasks_to_csv(user_id):
     if tasks_response.status_code != 200:
         return f"error fetching tasks "
     tasks_data = tasks_response.json()
-    csv_data = [["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"]]
-    for task in tasks_data:
-        csv_data.append([user_id, user_data["username"],
-                        task["completed"], task["title"]])
+    csv_data = [["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]]
 
+    for task in tasks_data:
+        csv_data.append([user_id, user_data["username"], task["completed"], task["title"]])
+
+   
     csv_file_name = f"{user_id}.csv"
     with open(csv_file_name, 'w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         writer.writerows(csv_data)
-
+    
     return f"Data exported successfully to {csv_file_name}."
 
-
-export_tasks_to_csv(2)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        user_id = sys.argv[1]
+        export_tasks_to_csv(user_id)
+    else:
+        print("Error: Please enter a user ID.")
