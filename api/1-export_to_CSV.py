2d4f340bb1699125dd92e33ff8b3f8ee8093fa34
cyrine <cyrinezekri4@gmail.com>
2023-11-24 23:21:35 +0100

    last attempt

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,53 +1,53 @@
 #!/usr/bin/python3
-''' Module documentation '''
+''' Module documentation'''
 import csv
 import requests
-import sys
+from sys import argv
+BASE_URL = 'https://jsonplaceholder.typicode.com'
 
 
 def export_tasks_to_csv(employee_id):
-    '''Export tasks to a CSV file for the given employee ID.'''
     try:
-        base_url = 'https://jsonplaceholder.typicode.com'
+        response = requests.get(f'{BASE_URL}/users/{employee_id}')
+        response.raise_for_status()
+        user_data = response.json()
+        employee_name = user_data.get('name')
 
-        user_response = requests.get(f'{base_url}/users/{employee_id}')
-        user_response.raise_for_status()
-        user = user_response.json()
-
-        todos_response = requests.get(
-            f'{base_url}/todos',
-            params={'userId': employee_id})
-        todos_response.raise_for_status()
-        todos = todos_response.json()
-
-        
-        tasks_data = [
-            ["USER_ID", "USERNAME", "EMPLOYEE_NAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        ]
-        for task in todos:
-            tasks_data.append([
-                employee_id,
-                user['username'],
-                user['name'],  
-                task['completed'],
-                task['title']
-            ])
+        response = requests.get(f'{BASE_URL}/todos', params={'userId': employee_id})
+        response.raise_for_status()
+        todos = response.json()
 
         csv_file_name = f"{employee_id}.csv"
-        with open(csv_file_name, 'w', newline='') as csv_file:
-            writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-            writer.writerows(tasks_data)
+        with open(csv_file_name, "w", newline='') as csv_file:
+            fieldnames = [
+                "USER_ID",
+                "USERNAME",
+                "EMPLOYEE_NAME",
+                "TASK_COMPLETED_STATUS",
+                "TASK_TITLE"
+            ]
+            writer = csv.DictWriter(
+                csv_file, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+            
+            writer.writeheader()
 
-        print(
-            f"Tasks for employee {user['name']} have been exported to {csv_file_name}.")
+            for task in todos:
+                writer.writerow({
+                    "USER_ID": employee_id,
+                    "USERNAME": user_data.get('username'),
+                    "EMPLOYEE_NAME": employee_name,
+                    "TASK_COMPLETED_STATUS": task['completed'],
+                    "TASK_TITLE": task['title']
+                })
+
+        print(f"Tasks for employee {employee_name} have been exported to {csv_file_name}.")
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: Please enter a user ID.")
-        sys.exit(1)
-    else:
-        user_id = sys.argv[1]
-        export_tasks_to_csv(user_id)
+    if len(argv) != 2:
+        exit(1)
+
+    employee_id = int(argv[1])
+    export_tasks_to_csv(employee_id)
