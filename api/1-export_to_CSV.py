eb299fd672e1b05a9d98d46431d10e42e226185c
cyrine <cyrinezekri4@gmail.com>
2023-11-24 23:29:32 +0100

    I give up

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,37 +6,39 @@ import sys
 
 
 def export_tasks_to_csv(employee_id):
-    '''Export tasks to a CSV file for the given employee ID.'''
-    try:
-        base_url = 'https://jsonplaceholder.typicode.com'
+    base_url = 'https://jsonplaceholder.typicode.com'
 
-        user_response = requests.get(f'{base_url}/users/{employee_id}')
-        user_response.raise_for_status()
+    user_response = requests.get(f'{base_url}/users/{employee_id}')
+    todos_response = requests.get(
+        f'{base_url}/todos',
+        params={
+            'userId': employee_id})
+
+    if user_response.status_code == 200 and todos_response.status_code == 200:
         user = user_response.json()
-
-        todos_response = requests.get(
-            f'{base_url}/todos',
-            params={'userId': employee_id})
-        todos_response.raise_for_status()
         todos = todos_response.json()
+
+        tasks_data = [
+            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        ]
+        for task in todos:
+            tasks_data.append([
+                employee_id,
+                user['username'],
+                task['completed'],
+                task['title']
+            ])
 
         csv_file_name = f"{employee_id}.csv"
         with open(csv_file_name, 'w', newline='') as csv_file:
-            writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
+            writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+            writer.writerows(tasks_data)
 
-            writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-            for task in todos:
-                writer.writerow([
-                    str(employee_id),
-                    user['username'],
-                    str(task['completed']),
-                    task['title']
-                ])
-
-        print(f"Tasks for employee {user['name']} have been exported to {csv_file_name}.")
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
+        print(
+            f"Tasks for employee {user['name']} "
+            f"have been exported to {csv_file_name}.")
+    else:
+        print("Failed to fetch data")
 
 
 if __name__ == "__main__":
